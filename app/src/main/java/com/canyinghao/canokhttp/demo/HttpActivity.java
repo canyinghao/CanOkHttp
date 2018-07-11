78ad9f14a4296a91d20565468625ba318fb1cd01
canyinghao <canyinghao@hotmail.com>
2018-07-11 15:32:19 +0800

    2.2.2

--- app/src/main/java/com/canyinghao/canokhttp/demo/HttpActivity.java
+++ app/src/main/java/com/canyinghao/canokhttp/demo/HttpActivity.java
@@ -76,7 +76,17 @@ public class HttpActivity extends BaseActivity {
 //                仅网络请求（NETWORK）
 
 
+                String someUrl  = CanOkHttp.getInstance()
+                        .add("app", "life.time")
+                        .add("appkey", "10003")
+                        .add("sign", "b59bc3ef6191eb9f747dd4e83c99f2a4")
+                        .add("format", "json")
+                        .add(String.valueOf("format"), "json")
+                        .url(url)
+                        .getFullUrl(true,0);
 
+
+           KLog.e(someUrl);
 
                 CanOkHttp.getInstance()
                         .add("app", "life.time")
